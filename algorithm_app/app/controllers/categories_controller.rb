4bdf6cc9bee70bc5f4fa68ac622edc1889bb5afa
clyon0409 <cecilia_lyon@sbcglobal.net>
2015-03-23 22:34:49 -0700

    modal dialog with embedded youtube video working

--- algorithm_app/app/controllers/categories_controller.rb
+++ algorithm_app/app/controllers/categories_controller.rb
@@ -1,5 +1,14 @@
 class CategoriesController < ApplicationController
 	def	index
+		@categories = Category.all
 	end
+
+	def show
+    	#respond_to do |format|
+      	#	format.html
+	    #	format.js
+	    #end
+  	end
+
 
 end
--- algorithm_app/config/routes.rb
+++ algorithm_app/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'categories#index'
 
+  get "categories/:id" => 'categories#show', :as => :category_show
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
