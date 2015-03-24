b33a20550f168d034fbdeb991e21ea4fa566debf
clyon0409 <cecilia_lyon@sbcglobal.net>
2015-03-24 11:12:17 -0700

    challenge buttons and status working, completed challenges count working

--- algorithm_app/app/controllers/categories_controller.rb
+++ algorithm_app/app/controllers/categories_controller.rb
@@ -1,13 +1,16 @@
 class CategoriesController < ApplicationController
 	def	index
 		@categories = Category.all
+		@total_challenges =  0
+		@total_completed = 0
+		@categories.each do |category|
+			@total_challenges = @total_challenges + category.challenges.count
+			@total_completed = @total_completed + category.challenges.where(status: 't').count
+		end
 	end
 
 	def show
-    	#respond_to do |format|
-      	#	format.html
-	    #	format.js
-	    #end
+    	
   	end
 
 
